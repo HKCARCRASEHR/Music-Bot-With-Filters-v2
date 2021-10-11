ca6dfa1280cd267d5d75e421063bf9679355aec3
HKCARCRASEHR <91190205+HKCARCRASEHR@users.noreply.github.com>
2021-10-11 17:40:53 +0800

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -21,7 +21,7 @@ module.exports = {
     let lyricsEmbed = new MessageEmbed()
       .setTitle("Lyrics")
       .setDescription(lyrics)
-      .setColor("#F0EAD6")
+      .setColor("#FF0000")
       .setTimestamp();
 
     if (lyricsEmbed.description.length >= 2048)
